b438b3106daf840bb79e7fbae7700717d8db4fb0
Surbhisahu7987 <149087282+Surbhisahu7987@users.noreply.github.com>
2024-07-26 13:40:19 +0530

    Integertoroman.cpp

---
+++ Integertoroman.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+     string roman="";
+     while(num>=1000)
+     {
+        roman=roman+"M";
+        num=num-1000;
+     }
+        if(num<1000 &&num>=900)
+        {
+            roman=roman+"CM";
+            num=num-900;
+        }
+     
+      while(num>=500)
+     {
+        roman=roman+"D";
+        num=num-500;
+     }
+        if(num<500 &&num>=400)
+        {
+            roman=roman+"CD";
+            num=num-400;
+        }
+     
+      while(num>=100)
+     {
+        roman=roman+"C";
+        num=num-100;
+     }
+        if(num<100 && num>=90)
+        {
+            roman=roman+"XC";
+            num=num-90;
+        }
+     
+      while(num>=50 )
+     {
+        roman=roman+"L";
+        num=num-50;
+     }
+        if(num<50 && num>=40)
+        {
+            roman=roman+"XL";
+            num=num-40;
+        }
+     
+      while(num>=10)
+     {
+        roman=roman+"X";
+        num=num-10;
+     }
+        if(num<10 && num==9)
+        {
+            roman=roman+"IX";
+            num=num-9;
+        }
+     
+      while(num>=5)
+     {
+        roman=roman+"V";
+        num=num-5;
+     }
+        if(num<5 &&num==4)
+        {
+            roman=roman+"IV";
+            num=num-4;
+        }
+     
+      while(num>=1)
+     {
+        roman=roman+"I";
+        num=num-1;
+        
+     }
+     return roman;
+    }
+};
